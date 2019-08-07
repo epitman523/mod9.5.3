9eb9e7f9671133ca0c47270cb72d5b1e090a3a15
Elizabeth Emig <epitman523@gmail.com>
2019-08-07 11:20:43 -0500

    finished js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 'use strict';
-
+function getDogLink() {
+    let dogBreed = $('.dog-list option:selected').val();
+    let doggieURL = 'https://dog.ceo/api/breed/' + dogBreed + '/images/random';
+    return doggieURL;
+}
 function getDogImages() {
-    //3) get number user entered
-    let userNumber = $('input').val();
-    if (!userNumber) {
-        userNumber = 3;
-    }
-    let doggieURL = 'https://dog.ceo/api/breeds/image/random/' + userNumber;
-    fetch(doggieURL)
+    let doggieLink = getDogLink();
+    fetch(doggieLink)
         .then(response => response.json())
         .then(responseJSON => showDogImg(responseJSON))
         .catch(error => console.log('Something went wrong'));
@@ -16,15 +15,19 @@ function buildString(item) {
     return `<li><img src="${item}" alt="image of cute dog" /></li>`;
 }
 function showDogImg(responseJSON) {
-    const messageArray = responseJSON.message;
-    const dogData = messageArray.map(message => buildString(message));
-    $('.response').removeClass('hidden');
-    console.log(messageArray);
-    $('.dogs').append(dogData);
+    const dogData = responseJSON.message;
+    const dogStatus = responseJSON.status;
+    if (dogStatus === 'error') {
+        $('.dogs').append('I am sorry, we do no have an image for that breed');
+    } else {
+        let dogString = buildString(dogData);
+        $('.dogs').append(dogString);
+    }
 }
 function watchDogForm() {
     $('form').submit(event => {
         event.preventDefault();
+        $('.dogs').empty();
         getDogImages();
     });
 }
