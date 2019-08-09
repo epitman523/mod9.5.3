6ee710fa943324dec41dba8835991e4a42bda714
Elizabeth Emig <epitman523@gmail.com>
2019-08-09 13:53:01 -0500

    still working on search

--- scripts.js
+++ scripts.js
@@ -1,35 +1,39 @@
 'use strict';
 
-let dogList = [];
+const dogsList = ["affenpinscher", "african", "airedale", "akita", "appenzeller", "basenji", "beagle", "bluetick", "borzoi", "bouvier", "boxer", "brabancon", "briard", "bulldog-boston", "bulldog-english", "bulldog-french", "bullterrier-staffordshire", "cairn", "cattledog-australian", "chihuahua", "chow", "clumber", "cockapoo", "collie-border", "coonhound", "corgi-cardigan", "cotondetulear", "dachshund", "dalmatian", "dane-great", "deerhound-scottish", "dhole", "dingo", "doberman", "elkhound-norwegian", "entlebucher", "eskimo", "frise-bichon", "germanshepherd", "greyhound-italian", "groenendael", "hound-afghan", "hound-basset", "hound-blood", "hound-english", "hound-ibizan", "hound-walker", "husky", "keeshond", "kelpie", "komondor", "kuvasz", "labrador", "leonberg", "lhasa", "malamute", "malinois", "maltese", "mastiff-bull", "mastiff-english", "mastiff-tibetan", "mexicanhairless", "mix", "mountain-bernese", "mountain-swiss", "newfoundland", "otterhound", "papillon", "pekinese", "pembroke", "pinscher-miniature", "pointer-germanlonghair", "pomeranian", "poodle-miniature", "poodle-standard", "poodle-toy", "pug", "puggle", "pyrenees", "redbone", "retriever-chesapeake", "retriever-curly", "retriever-flatcoated", "retriever-golden", "ridgeback-rhodesian", "rottweiler", "saluki", "samoyed", "schipperke", "schnauzer-giant", "schnauzer-miniature", "setter-english", "setter-gordon", "setter-irish", "sheepdog-english", "sheepdog-shetland", "shiba", "shihtzu", "spaniel-blenheim", "spaniel-brittany", "spaniel-cocker", "spaniel-irish", "spaniel-japanese", "spaniel-sussex", "spaniel-welsh", "springer-english", "stbernard", "terrier-american", "terrier-australian", "terrier-bedlington", "terrier-border", "terrier-dandie", "terrier-fox", "terrier-irish", "terrier-kerryblue", "terrier-lakeland", "terrier-norfolk", "terrier-norwich", "terrier-patterdale", "terrier-russell", "terrier-scottish", "terrier-sealyham", "terrier-silky", "terrier-tibetan", "terrier-toy", "terrier-westhighland", "terrier-wheaten", "terrier-yorkshire", "vizsla", "weimaraner", "whippet", "wolfhound-irish"];
+const dogsListTest = transformDogs(dogsList);
 
-function getDogList() {
-    fetch('https://dog.ceo/api/breeds/list/all')
-        .then(response => response.json())
-        .then(responseList => convertDogList(responseList))
-        .catch(error => console.log('Something wrong with getDogList'));
+function transformDogs(arr) {
+    let newArr = arr.map(function (item) {
+        let newWord = item.replace('-', '').split('').sort().join('');
+        return newWord;
+    })
+    return newArr;
+}
+function transformInput(str) {
+    let newString = str.toLowerCase().replace('/\W/g', '').split('').sort().join('');
+    return newString;
+}
+function getUserInput() {
+    let userInput = $('#dogForm input').val();
+    return transformInput(userInput);
 }
 
-function convertDogList(responseList) {
-    //1. Define responseList
-    let rList = responseList.message;
-    console.log(rList.length);
+function searchUserInput() {
+    let searchTerm = getUserInput();
 
-    //2. map through responseList
+    for (let i = 0; i > dogsListTest.length; i++) {
+        if (dogsListTest[i] === searchTerm) {
+            return dogList[i]
+        } else {
+            return false;
+        }
 
-    //3. Obtain value of message[i]
-    //4. check if message[i][0] is true
-
-
-
-
-    //6. build appropriate string
-    //7. if false 
-    //8. build appropriate string
+    }
 }
-
 function getDogLink() {
-    let dogBreed = $('.dog-list option:selected').val();
-    let doggieURL = 'https://dog.ceo/api/breed/' + dogBreed + '/images/random';
+
+    let doggieURL = 'https://dog.ceo/api/breed/' + key + '/images/random';
     return doggieURL;
 }
 function getDogImages() {
@@ -56,8 +60,8 @@ function watchDogForm() {
     $('form').submit(event => {
         event.preventDefault();
         $('.dogs').empty();
-        getDogList();
-        getDogImages();
+        getDogLink();
+        // getDogImages();
     });
 }
 $(function () {
